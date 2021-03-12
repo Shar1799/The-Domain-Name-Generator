573a317c8db5900a64066f4e0fd4345a7798c4a7
Shar1799 <sharmey99@gmail.com>
2021-03-12 23:19:55 +0000

    Nombre dominio

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Arreglos con las excusas
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  document.write("The Domain Name Generator: ");
+  document.write("<br>");
+  document.write("<br>");
+
+  for (let i = 0; i < 10; i++) {
+    //Generador de número aleatorio
+    let sujeto = pronoun[Math.floor(Math.random() * pronoun.length)];
+    let adjetivo = adj[Math.floor(Math.random() * adj.length)];
+    let sustantivo = noun[Math.floor(Math.random() * noun.length)];
+
+    //Impresión del nombre en la página
+    document.write(sujeto + adjetivo + sustantivo + ".com");
+    document.write("<br>");
+  }
+
+  /*
+  //Generador de número aleatorio
+  let sujeto = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let adjetivo = adj[Math.floor(Math.random() * adj.length)];
+  let sustantivo = noun[Math.floor(Math.random() * noun.length)];
+
+  //Impresión del nombre en la página
+  document.write(sujeto + adjetivo + sustantivo + ".com");*/
 };
